45329eec3b0ea18377758ab3e933512eae31c44e
joshu192 Kerr <joshkerr192@gmail.com>
2025-02-04 17:44:05 +0000

    comment

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -5,7 +5,10 @@ ask prompt =
   line <- getLine
   if line == ""
     then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    else if(line == "quit") then putStrLn("quitting") --condition added for quitting
+    else do
+      else putStrLn ("you said: " ++ reverse line)
+      ask prompt --needed to stop program stopping after printing reverse line
 
 main :: IO ()
 main =
